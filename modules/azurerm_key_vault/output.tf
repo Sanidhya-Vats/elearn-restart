905ea6276cf48ad2796ecfd892741d26a7f05a98
inferno-8 <rahuldevopseng@gmail.com>
2026-01-12 18:50:35 +0530

    FEAT : Update key vault output references and comment out URI
    Changed output references from 'azurerm_key_vault.this' to 'azurerm_key_vault.key_vault'.
    Commented out the 'key_vault_uri' output with a note to revisit when private endpoint support is implemented.

--- modules/azurerm_key_vault/output.tf
+++ modules/azurerm_key_vault/output.tf
@@ -1,11 +1,14 @@
 output "key_vault_id" {
-  value = azurerm_key_vault.this.id
+  value = azurerm_key_vault.key_vault.id
 }
 
 output "key_vault_name" {
-  value = azurerm_key_vault.this.name
+  value = azurerm_key_vault.key_vault.name
 }
 
-output "key_vault_uri" {
-  value = azurerm_key_vault.this.vault_uri
-}
+
+
+//TODO : DNS endpoint (URL) when private endpoint is implemented
+  # output "key_vault_uri" {
+  #   value = azurerm_key_vault.this.vault_uri
+  # }
